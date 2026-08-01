5e82b43c56696cc8d35e00de89feaf0c1b7983f8
parthraj27 <parthraj27@gmail.com>
2026-08-02 02:30:28 +0530

    feat: add organizationId to ProductFilter, ListProductsQuery, and update search/list methods in ProductsController

--- src/application/modules/products/domain/product.filter.ts
+++ src/application/modules/products/domain/product.filter.ts
@@ -4,4 +4,5 @@ export class ProductFilter {
   @AutoMap() public name?: string;
   @AutoMap() public categoryId?: string;
   @AutoMap() public isActive?: boolean;
+  @AutoMap() public organizationId?: string;
 }
