31fb3dc5ccc8bf15791e5cf2a08986e557835ba6
parthraj27 <parthraj27@gmail.com>
2026-09-01 23:10:54 +0530

    feat: add migration for pack_quantity and pack_size fields in purchase_items and order_items tables

--- src/infrastructure/persistence/migrations/index.ts
+++ src/infrastructure/persistence/migrations/index.ts
@@ -2,6 +2,7 @@
 export * from './1787251424817-migration';
 export * from './1787392406941-migration';
 export * from './1787630908150-migration';
+export * from './1787681135660-migration';
 export * from './1788028557892-migration';
 export * from './1788068982880-migration';
 export * from './1788081078461-migration';
