651c5ec3367b4b111c43a433044a967467524c84
Parth Rajyaguru <139099238+ParthRaj27@users.noreply.github.com>
2026-09-08 00:18:30 +0530

    Migration fix

--- src/infrastructure/persistence/migrations/index.ts
+++ src/infrastructure/persistence/migrations/index.ts
@@ -9,4 +9,4 @@ export * from './1788081078461-migration';
 export * from './1788084835696-migration';
 export * from './1788252500000-migration';
 export * from './1788283790698-migration';
-export * from './1788400000000-migration';
+export * from './1788806546993-migration'
