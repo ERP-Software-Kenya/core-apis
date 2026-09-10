87192a9010725b2c7c262439531322c2198a5ef3
parthraj27 <parthraj27@gmail.com>
2026-09-10 23:05:21 +0530

    feat(migrations): add missing migration exports to index file

--- src/infrastructure/persistence/migrations/index.ts
+++ src/infrastructure/persistence/migrations/index.ts
@@ -7,3 +7,7 @@ export * from './1788028557892-migration';
 export * from './1788068982880-migration';
 export * from './1788081078461-migration';
 export * from './1788084835696-migration';
+export * from './1788252500000-migration';
+export * from './1788283790698-migration';
+export * from './1788806546993-migration';
+export * from './1789059180558-migration';
