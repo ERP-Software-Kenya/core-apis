9ea290dcb5929696f65fc5b12fc0c095fa8dd3b0
Hitarth92 <mangukiyahitarth8@gmail.com>
2026-09-17 14:58:30 +0530

    chore: remove dev auth bypass from clerk guard and common module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -6,8 +6,6 @@ import { JwtService } from '@nestjs/jwt';
 import { PUSH_NOTIFICATION_SERVICE, PushNotificationService } from './push-notification';
 import { PDF_EXPORT_SERVICE, PdfExportService } from './pdf-export';
 
-import { ClerkAuthGuard } from './auth';
-
 @Global()
 @Module({
   providers: [
@@ -27,13 +25,11 @@ import { ClerkAuthGuard } from './auth';
     FileParserService,
     CentrifugalService,
     JwtService,
-    ClerkAuthGuard,
   ],
   exports: [
     CentrifugalService,
     PUSH_NOTIFICATION_SERVICE,
     PDF_EXPORT_SERVICE,
-    ClerkAuthGuard,
   ],
 })
 export class CommonModule {}
